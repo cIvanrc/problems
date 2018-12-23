662153cb0174e72679cca064a8bb9f3c44e7c691
Ivan <c.ivan.felix.r@gmail.com>
2018-12-23 09:03:40 -0600

    guess_game.py

---
+++ condition_and_loop/guess_game.py
@@ -0,0 +1,22 @@
+# Generate a random number between 1 and 9 (including 1 and 9).
+# Ask the user to guess the number, then tell them whether they guessed too low, too high, or exactly right.
+
+from random import randint
+
+def guess_game():
+    i = randint(1,9)
+
+    print(check_response(i))
+
+def check_response(i):
+    number = int(input("Digit a number: "))
+    output = ''
+    if i < number:
+        output = "too low"
+    elif i > number:
+        output = "too high"
+    else:
+        output = "exactly rigth"
+    return output
+
+guess_game()
