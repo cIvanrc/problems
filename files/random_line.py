c173236339153278f78e68dfb2b263598b40d905
ivan <c.ivan.felix.r@gmail.com>
2019-01-08 02:51:52 -0600

    random_line.py testing aliases daoshdo dasijdoa

---
+++ files/random_line.py
@@ -0,0 +1,8 @@
+#Write a program to read a random line from a file
+
+
+import random
+
+def random_line(file_name):
+    lines = open(file_name).read().splitlines()
+    return random.choice(lines)
