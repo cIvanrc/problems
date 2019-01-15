33902a74d153c75b6a4c8ca4e0257eaf3f24f4e9
ivan <c.ivan.felix.r@gmail.com>
2019-01-15 01:17:24 -0600

    stantard_deviation.py

---
+++ math/standar_deviation.py
@@ -0,0 +1,39 @@
+# write a python program to calculate the standar deviation of the following data
+# input
+# sample data: [4,2,5,8,6]
+# output
+# satandar deviation: 2.23606797749979
+
+
+
+import math
+import sys
+
+def sd_calc(data):
+    n = len(data)
+    if n <= 1:
+        return 0.0
+
+    mean, sd = avg_calc(data), 0.0
+
+    for el in data:
+        sd += (float(el) - mean)**2
+    sd = math.sqrt(sd / float(n-1))
+
+    return sd
+
+def avg_calc(ls):
+    n, mean = len(ls), 0.0
+    if n <= 1:
+        return ls[0]
+
+    for el in ls:
+        mean = mean + float(el)
+
+    mean = mean/float(n)
+    return mean
+
+data = [4,2,5,8,6]
+print("Sample data: ",data)
+print("Standard deviation:", sd_calc(data))
+
