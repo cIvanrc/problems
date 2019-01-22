0bcc95b7175460d90c867757ee3785a8681c75fa
ivan <c.ivan.felix.r@gmail.com>
2019-01-22 00:42:37 -0600

    array_rotate.py

---
+++ array/array_rotate.py
@@ -0,0 +1,20 @@
+# Rotate an array of n elements to the right by k steps
+# For example, with n = 7 and k = 3
+# the array [1,2,3,4,5,6,7] is rotated to [5,6,7,1,2,3,4]
+# Note:
+# Try to come up as many solutions as you can
+# there are at least 3 different ways to solve this problem
+
+org = [1,2,3,4,5,6,7]
+
+result = org[:]
+
+steps = 3
+
+for idx, num in enumerate(org):
+    if idx+steps < len(org):
+        result[idx+steps] = org[idx]
+    else:
+        result[idx+steps - len(org)] = org[idx]
+
+print(result)
