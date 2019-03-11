93c200028c0ffb8e9698864c01429d3ec41c3256
ivan <c.ivan.felix.r@gmail.com>
2019-03-10 22:36:02 -0600

    input_age.rb

--- 1_basic/input_age.rb
+++ 1_basic/input_age.rb
@@ -1,19 +1,19 @@
-# Create a program that asks the user to enter their name and their age.
-# Print out a message addressed to them that tells them the year they will turn 100 years old.
+require 'date'
 
-import datetime
+def input_age
+   print "your name: "
+   name = gets.chomp
+   print "your age: "
+   age = gets.chomp.to_i
 
-def input_age():
-    name = input("your name: ")
-    age = int(input("your age: "))
-    current_year = get_current_year()
+   current_year = get_current_year
 
-    finally_year = 100 - age + current_year
-    print("{}, in the year {} you will turn 100 years old".format(name, finally_year))
+   finally_year = 100 - age + current_year
 
-                    
-def get_current_year():
-    now = datetime.datetime.now()
-    return now.year
+   puts "#{name}, in the year #{finally_year} you will turn 100 years old"
+end
 
-input_age()
+def get_current_year
+   now = DateTime.now
+   now.strftime("%Y").to_i
+end
