09cbcfb6da6ce0a4fa028314d707e3a2a9b07e53
ivan <c.ivan.felix.r@gmail.com>
2019-03-27 14:54:49 -0600

    accessing a private method with instance_eval

--- ruby_meta/instance_eval.rb
+++ ruby_meta/instance_eval.rb
@@ -3,14 +3,21 @@ class Thing
   def initialize
     @var = 99
   end
+
+  private
+
+  def secret
+    puts "a method private"
+  end
 end
 
 
 t = Thing.new
 
-puts t.instance_eval("@var")
-puts t.instance_eval { @var } 
+t.instance_eval { secret }
+#puts t.instance_eval("@var")
+#puts t.instance_eval { @var } 
 
 
-t.instance_eval { @var = "cat" }
-puts t.instance_eval { @var }
+#t.instance_eval { @var = "cat" }
+#puts t.instance_eval { @var }
