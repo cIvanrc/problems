f40823bbd79583786a3adc5f2c1dbc04afa09889
ivan <c.ivan.felix.r@gmail.com>
2019-04-12 10:49:01 -0500

    Ruby: binary search

---
+++ ruby_and_python/13_search/binary_search.rb
@@ -0,0 +1,36 @@
+class Search
+
+  def initialize(numbers, number)
+    @numbers = numbers
+    @number = number
+  end
+
+  def binary_search
+    first = 0
+    last = @numbers.size - 1
+
+    while first <= last
+
+      mid = ( first + last ) / 2
+      if @numbers[mid] == @number
+
+        return "Si"
+      end
+
+      if @numbers[mid] < @number
+        first = mid + 1
+      else
+        last = mid - 1
+      end
+    end
+
+    return "No"
+
+  end
+end
+
+
+search = Search.new([1,4,6,9,106,305,468,563,693], 468)
+#search = Search.new([1,4,6,9,106,305,468,563,693], 346)
+puts search.binary_search
+
