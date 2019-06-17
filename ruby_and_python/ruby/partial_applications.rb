e5c77c6c8eddaf560eb286f7e1af5245c97a4db7
ivan <c.ivan.felix.r@gmail.com>
2019-06-17 16:53:29 -0500

    Ruby: partial applications

---
+++ ruby_and_python/ruby/partial_applications.rb
@@ -0,0 +1,36 @@
+# In Partial Application, we create a lambda that takes a parameter and returns a lambda that does something with it.
+
+# Example:
+
+# multiply_function = -> (number) do
+   # -> (another_number) do
+       # number * another_number
+   # end
+# end
+
+# doubler = multiply_function.(2)
+# tripler = multiply_function.(3)
+
+# puts doubler.(4)
+# puts tripler.(4)
+# In the above example, the lambda will take number as a parameter, and return a lambda. When you call this lambda with another_number, it will return the product of the two.
+
+# Task
+
+# You are given a partially complete code. Your task is to fill in the blanks (_______).
+
+# Here, combination is a variable that stores a partial application which computes combination .
+#
+
+
+
+combination = -> (number) do
+    -> (another_number) { 
+        (number-another_number+1..number).inject(:*) / (1..another_number).inject(:*) 
+    }
+end
+
+n = gets.to_i
+r = gets.to_i
+nCr = combination.(n)
+puts nCr.(r)
