c3101d69506e32538a1b92f2d2bf14a7f524b3e2
ivan <c.ivan.felix.r@gmail.com>
2019-06-25 12:17:31 -0500

    Ruby: testing => raise_error

--- testing/ruby_testing/example_rspec_tdd/lib/calculator.rb
+++ testing/ruby_testing/example_rspec_tdd/lib/calculator.rb
@@ -3,5 +3,9 @@ class Calculator
   def sum(a, b)
     a+b
   end
+
+  def div(a, b) 
+    a/b
+  end
 end
 
--- testing/ruby_testing/example_rspec_tdd/spec/calculator/calculator_spec.rb
+++ testing/ruby_testing/example_rspec_tdd/spec/calculator/calculator_spec.rb
@@ -3,6 +3,15 @@ require 'calculator'
 describe Calculator, ":About a Calculator" do
   subject(:cal) { Calculator.new }
 
+  context '#div' do
+    it 'error: divide by 0' do
+      expect{ cal.div(3,0) }.to raise_error(ZeroDivisionError)
+      expect{ cal.div(3,0) }.to raise_error("divided by 0")
+      expect{ cal.div(3,0) }.to raise_error(ZeroDivisionError,"divided by 0")
+      expect{ cal.div(3,0) }.to raise_error(/divided/)
+    end
+  end
+
   context '#sum' do
     it "with positive numbers" do
       result = cal.sum(2,5)
